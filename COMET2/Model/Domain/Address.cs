c32843f054e2ed2c83fa06436d8180fd33617d30
Tah Tatsumoto <tah.tatsumoto@icloud.com>
2015-10-14 00:17:52 -0600

    Addition of Address Class

---
+++ COMET2/Model/Domain/Address.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMET.Model.Domain {
+    public class Address {
+
+        public string PrefixDirection {
+			private get;
+			set;
+		}
+    }
+}
